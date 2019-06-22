05401a8d6ad2428e1515fcb2f8715273e78fd582
Le Bin Ho <bin262@gmail.com>
2019-06-22 16:59:50 +0900

    Create __init__.py

---
+++ qsim/__init__.py
@@ -0,0 +1,32 @@
+# This file is a part of QSim: Quantum computor Simulation code.
+# Copyright (c) 2019 and later
+# Authors: Binho Le 
+# All rights reserved.
+
+import os
+import sys
+import warnings
+
+#import qutip.settings
+import qsim.version
+from qsim.version import version as __version__
+
+# ------------------------------------------
+# Load modules
+# ==========================================
+
+# utilities
+from qsim.utilities import *
+from qsim.about import *
+
+# outer loop
+from qsim.qustates import *
+from qsim.cdf import *
+
+# quantum state tomography
+from qsim.dsm import *
+
+# -----------------------------------------------------------------------------
+# Clean name space
+#
+del os, sys#, numpy, scipy, multiprocessing, distutils
